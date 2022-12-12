0075de99cbf189be41f7df19ecb0fdedf24a75a9
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-12 18:25:39 -0300

    feat: :sparkles: add local storage helper

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,3 +1,18 @@
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
 }
+
+export function lstorage(
+  op: 'get' | 'set',
+  key: string,
+  value: unknown = null
+) {
+  const valueStorage = localStorage.getItem(key)
+
+  if (op === 'get') {
+    if (!valueStorage) return null
+    return JSON.parse(valueStorage)
+  } else if (op === 'set') {
+    localStorage.setItem(key, JSON.stringify(value))
+  }
+}
