0f8458570a4fe1c700fb33ca0c6077239b923908
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-12 15:56:33 -0300

    feat: :sparkles: add homepage layout

--- app/src/components/App.tsx
+++ app/src/components/App.tsx
@@ -1,26 +1,19 @@
-import Avatar from 'components/Avatar'
+import { RecoilRoot } from 'recoil'
+import Contact from './Contact'
+import Header from './Header'
 
 function App() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-screen-xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold uppercase tracking-wide text-blue-600">
-            Welcome to
-          </h2>
-          <p className="my-3 text-4xl font-bold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            reactjs-vite-tailwindcss-boilerplate
-          </p>
-          <p className="text-xl text-gray-400">Start building for free.</p>
-          <p className="mt-5">
-            <Avatar
-              size="large"
-              src="https://www.gravatar.com/avatar/4405735f6f3129e0286d9d43e7b460d0"
-            />
-          </p>
-        </div>
-      </div>
-    </div>
+    <RecoilRoot>
+      <section className="flex h-screen flex-col items-center bg-blue-400 pt-16">
+        <Header />
+
+        <span className="mb-4 text-4xl text-white">
+          Find new users like you
+        </span>
+        <Contact />
+      </section>
+    </RecoilRoot>
   )
 }
 
