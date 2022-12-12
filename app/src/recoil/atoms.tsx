19398da53aac180284d972f2fde84314c3078ada
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-12 18:24:59 -0300

    feat: :sparkles: add new global state

--- app/src/recoil/atoms.tsx
+++ app/src/recoil/atoms.tsx
@@ -5,3 +5,8 @@ export const currentPerfilState = atom<Perfil | null>({
   key: 'currentPerfilState', // unique ID (with respect to other atoms/selectors)
   default: null // default value (aka initial value)
 })
+
+export const followersState = atom<Perfil[] | []>({
+  key: 'followersState', // unique ID (with respect to other atoms/selectors)
+  default: [] // default value (aka initial value)
+})
