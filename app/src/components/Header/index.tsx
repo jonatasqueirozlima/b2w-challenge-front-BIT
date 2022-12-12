842e78c0bf72e612a1c644bbd23b08fab5c59668
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-12 15:53:55 -0300

    feat: :sparkles: add Header

---
+++ app/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="fixed top-0 bg-blue-400 w-screen">
+      <div className="w-6/12 mx-auto py-2 text-white flex justify-between">
+        <span className="">users_like.me</span>
+        <span>...</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
