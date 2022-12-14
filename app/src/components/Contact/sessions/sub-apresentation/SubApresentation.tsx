bd7274ebd05850d208e37f94e1f63916b6cfd381
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-14 09:16:19 -0300

    refactor: :recycle: remove info prop
    use perfil global state

--- app/src/components/Contact/sessions/sub-apresentation/SubApresentation.tsx
+++ app/src/components/Contact/sessions/sub-apresentation/SubApresentation.tsx
@@ -1,22 +1,23 @@
-import { FC, useEffect, useState } from 'react'
-import { Perfil } from 'types'
+import { FC } from 'react'
+import { useRecoilState } from 'recoil'
+import { currentPerfilState } from 'recoil/atoms'
 import ContactInfo from './ContactInfo'
 import PersonalInfo from './PersonalInfo'
 
 type Props = {
   title: string
-  info: Perfil
 }
 
-const SubApresentation: FC<Props> = ({ title, info }) => {
+const SubApresentation: FC<Props> = ({ title }) => {
+  const [perfil] = useRecoilState(currentPerfilState)
   const isContactInfo = title === 'Contact Info'
 
   return (
     <section className="h-fit border border-gray-500 bg-white p-4">
       <span className="font-bold">{title}</span>
 
-      {isContactInfo && info && <ContactInfo info={info} />}
-      {!isContactInfo && info && <PersonalInfo info={info} />}
+      {isContactInfo && perfil && <ContactInfo info={perfil} />}
+      {!isContactInfo && perfil && <PersonalInfo info={perfil} />}
     </section>
   )
 }
