4225089083e4506fce431ef0660571e8547dc45e
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-14 09:55:53 -0300

    chore: :fire: remove empty className

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 w-screen bg-blue-400">
       <div className="mx-auto flex w-6/12 justify-between py-4 text-white">
-        <span className="">users_like.me</span>
+        <span>users_like.me</span>
         <FollowerList />
       </div>
     </header>
