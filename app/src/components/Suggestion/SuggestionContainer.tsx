92164aa1667599d3dc63de2959084fc6a41c96a6
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-14 10:07:49 -0300

    feat: :sparkles: add message when no suggestions

--- app/src/components/Suggestion/SuggestionContainer.tsx
+++ app/src/components/Suggestion/SuggestionContainer.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 
 type Props = {
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element[]
 }
 
 const SuggestionContainer: FC<Props> = ({ children }) => {
@@ -9,7 +9,13 @@ const SuggestionContainer: FC<Props> = ({ children }) => {
     <section className="mt-6 w-6/12">
       <span className="text-2xl font-bold text-white">Suggestion 4you</span>
       <section className="mt-2 flex gap-x-3 py-3 overflow-x-auto">
-        {children}
+        {children.length === 0 ? (
+          <section className="bg-white w-full p-4">
+            <p>No suggestions for now</p>
+          </section>
+        ) : (
+          children
+        )}
       </section>
     </section>
   )
